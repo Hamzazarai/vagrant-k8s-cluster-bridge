c26c22f2da1f2d5c5611a0c8f514597507a83310
Hamza Zarai <hamzazarai11@gmail.com>
2025-10-22 22:18:13 +0100

    Update Kubernetes installation method in bootstrap scripts

--- pc1-master-workers/scripts/bootstrap.sh
+++ pc1-master-workers/scripts/bootstrap.sh
@@ -11,11 +11,15 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 # Installer kubeadm, kubelet, kubectl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo mkdir -p /etc/apt/keyrings/
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
 
 # Désactiver le swap
 sudo swapoff -a
--- pc2-workers/scripts/bootstrap.sh
+++ pc2-workers/scripts/bootstrap.sh
@@ -6,11 +6,16 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-relea
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# Installer kubeadm, kubelet, kubectl
+sudo mkdir -p /etc/apt/keyrings/
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
 
 sudo swapoff -a
 sudo sed -i '/ swap / s/^/#/' /etc/fstab
