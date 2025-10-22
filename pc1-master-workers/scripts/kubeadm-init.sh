5e27d6466bd2bfe86b497d68dd3e6c416513e338
Hamza Zarai <hamzazarai11@gmail.com>
2025-10-22 23:39:18 +0100

    Enhance kubeadm initialization script with  Flannel CNI application

--- pc1-master-workers/scripts/kubeadm-init.sh
+++ pc1-master-workers/scripts/kubeadm-init.sh
@@ -3,17 +3,26 @@ set -e
 
 MASTER_IP="192.168.1.10"
 
-# Initialisation du master
+echo "===== Initializing Kubernetes master ====="
 sudo kubeadm init --apiserver-advertise-address=$MASTER_IP --pod-network-cidr=10.244.0.0/16
 
-# Configurer kubectl pour l'utilisateur vagrant
+echo "===== Setting up kubectl for vagrant user ====="
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# Installer Flannel comme CNI
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+echo "===== Waiting for kube-apiserver to be ready ====="
+# Retry loop jusqu'à ce que kube-apiserver soit opérationnel
+until kubectl get nodes >/dev/null 2>&1; do
+  echo "Waiting for kube-apiserver..."
+  sleep 5
+done
 
-# Afficher la commande join
-echo "===== Run this command on worker nodes ====="
-kubeadm token create --print-join-command
+echo "===== Applying Flannel CNI ====="
+# Utiliser --validate=false pour éviter les erreurs liées au openapi
+kubectl apply --validate=false -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+
+echo "===== Generating join command for workers ====="
+JOIN_CMD=$(kubeadm token create --print-join-command)
+echo $JOIN_CMD
+echo "===== Copy the above command to run on your worker nodes ====="
